65990adc88c07d3e476f31f52896431914f6bbc7
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-08-11 22:42:41 +0300

    Added task from 5 kyu

---
+++ JavaScript/5kyu/movingZerosToTheEnd.js
@@ -0,0 +1,17 @@
+/* Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
+
+function moveZeros(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  }
+
+  return arr;
+}
+
+moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]);
+moveZeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0]);
